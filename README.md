e397b8f07e005cafe8027fe30ce63e78e08ecb38
Chang Chia Ming <skypc785308@gmail.com>
2018-07-16 17:08:48 +0800

    2018.07.16 更新基本訊息

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ecpay_invoice
 建置中，ECPAY電子發票Python模組
+
+如何安裝：
+利用pip
+
+.. code-block:: python
+    pip install ecpay_invoice
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 import io
 
-VERSION = '0.7'
+VERSION = '0.8'
 
 with io.open("README.md", encoding='utf-8') as f:
     long_description = f.read()
